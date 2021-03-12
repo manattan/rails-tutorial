692ad8885750e69ddf2dc3a70e040d107f2062d7
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-13 01:15:42 +0900

    記事にコメントを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    puts @article
+    puts @article.inspect
     @comment = @article.comments.create(comment_params)
+    puts @comment.inspect
     redirect_to article_path(@article)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
     end
-  end
 end

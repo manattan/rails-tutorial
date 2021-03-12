9d50ac1b0d26a5486a3fb9380807aada8e6b8194
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-13 01:03:25 +0900

    controller 改善

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    puts @article
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+  end
 end
